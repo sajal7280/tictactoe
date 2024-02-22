aa65179a83eb8911d9f684c536908a570d5c9252
Sajal <www.sajalkumar001@gmail.com>
2024-02-22 18:32:18 +0530

    First Commit

---
+++ src/main/java/com/abhyudayasharma/tictactoe/Main.java
@@ -0,0 +1,18 @@
+package com.abhyudayasharma.tictactoe;
+
+/**
+ * Execution starts here.
+ *
+ * @author Abhyudaya Sharma
+ */
+public class Main {
+    /**
+     * The 'main' method for the game.
+     *
+     * @param args the command line arguments.
+     */
+    public static void main(String[] args) {
+        new Game().start();
+    }
+}
+
